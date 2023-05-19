104023db1787ae224d01a4e5737e5752b90f4b0a
Reyas Khan M <119721119+rewyekha@users.noreply.github.com>
2023-05-19 23:32:35 +0530

    Database.py

---
+++ Object-oriented Programming, OOP/Employe Database-OOPs/Database.py
@@ -0,0 +1,20 @@
+class Employees:
+  def __init__(self, name, job):
+    seld.name = name
+    self.job = job
+
+  def tasks(selfs):
+    print("Tasks are:")
+
+class Manager(Employee):
+  def __init__(self, name, job, staff):
+    super().__init__(name, job) 
+    self.staff = staff
+
+  def tasks(self):
+    print("oversees:")
+    print(staff)
+
+class Associate(Employee):
+  def tasks(self):
+    print("Take orders from manager")
